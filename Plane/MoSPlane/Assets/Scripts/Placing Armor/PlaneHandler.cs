680b0c3c5929cd6893a153010c4b7177b5677200
HansEdward <mail@hans-edward.com>
2021-04-03 18:34:20 +0100

    Added basic flight testing. This functions by looking at the amount of armor the plane has in a specific location and then using that to do a calculation to see if it takes damage. If it doesn't nothing happens, if it does it will check the planes "damage count" (adjusted for critical areas) and if the damage count is greater than 10 (can be changed) it will "crash"
    Numbers I got when testing
    No armor
    Avg 31 rounds
    1 armor in both critical locations
    Avg 41,1666... rounds
    2 armor in both critical locations
    Avg 43,1666... rounds
    1 armor on every location
    Avg 49 rounds.
    
    all averages are off of 6 "runs" with each configurations. More testing is required

--- Plane/MoSPlane/Assets/Scripts/Placing Armor/PlaneHandler.cs
+++ Plane/MoSPlane/Assets/Scripts/Placing Armor/PlaneHandler.cs
@@ -8,7 +8,18 @@ public class PlaneHandler : MonoBehaviour
     public int[] armorArray = new int[10]; //This armor array will keep track of where the armor has been placed. This will impact the probability that the plane is being shot down. 
                                            //https://drive.google.com/file/d/1qvycwF34yzFbsOf40-u97HuM0XQ3fR4d/view?usp=sharing
                                            //1 = 0, 2 = 1, etc
+    public int[] damageArray = new int[10];
+
     public GameObject armorButtonsPNL;
+    public int chanceToGetHit = 1, rollMax = 2; //chanceToGetHit is the number that has to be beaten by the generator in order for the plane to calculate a hit
+                                                //rollMax is the maximum number the generator can roll (the random.range runs on the top number being exclusive so if the numbers are 0,2 it can generate 1 or 0, but not 2)
+    
+    public int damageChance = 2; //this is the number you add in order to calculate if the plane takes damage during a hit. Currently it adds to the amount of armor the plane has in the random.range function
+                                 //making the top number X bigger than the amount of armor. If the number generated is bigger than the amount of armor it takes damage. 
+    public int damageMultiplier = 3;
+    public int killNum = 10;
+    public bool hasStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +29,10 @@ public class PlaneHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasStarted == true)
+        {
+            FlightCalculator();
+        }
     }
 
     public void AddArmor(int armorPosition)
@@ -26,5 +40,61 @@ public class PlaneHandler : MonoBehaviour
         armorArray[armorPosition] += 1;
         Debug.Log(armorArray[armorPosition]);
         armorButtonsPNL.SetActive(false);
+    }
+
+    public void FlightCalculator()
+    {
+        int randHit = Random.Range(0, rollMax);
+        int randHitArea = Random.Range(0, armorArray.Length);
+        int randDamage = Random.Range(0, armorArray[randHitArea] + damageChance);
+        if (randHit >= chanceToGetHit)
+        {
+            if (armorArray[randHitArea] < randDamage)
+            {
+                damageArray[randHitArea] += 1;
+            }
+        }
+        
+        int dmgNum = 0;
+        
+        for (int i = 0; i < damageArray.Length; i++)
+        {
+            int dmgToAdd = 0;
+
+            if (i == 0 || i == 2)
+            {
+                dmgToAdd += damageArray[i] * damageMultiplier;
+            }
+            else
+            {
+                dmgToAdd += damageArray[i];
+            }
+
+            dmgNum += dmgToAdd;
+        }
+
+        if (dmgNum > killNum)
+        {
+            Debug.LogWarning("Player dead");
+            DamageArrayResetter();
+            hasStarted = !hasStarted;
+        }
+        else
+        {
+            Debug.Log("Player not dead");
+        }
+    }
+
+    public void DamageArrayResetter()
+    {
+        for (int i = 0; i < damageArray.Length; i++)
+        {
+            damageArray[i] = 0;
+        }
+    }
+
+    public void HasStartedSwitcher()
+    {
+        hasStarted = !hasStarted;
     }
 }
