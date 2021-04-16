452a3272c44ece73e3e2c12f1b570b7ac225ea08
HansEdward <mail@hans-edward.com>
2021-04-16 18:22:52 +0200

    Made changes to the function for the planes. This was for testing purposes only but I didn't yet test it out. Will do that and get back with the results

--- Plane/MoSPlane/Assets/Scripts/Placing Armor/PlaneHandler.cs
+++ Plane/MoSPlane/Assets/Scripts/Placing Armor/PlaneHandler.cs
@@ -13,10 +13,12 @@ public class PlaneHandler : MonoBehaviour
     public GameObject armorButtonsPNL;
     public int chanceToGetHit = 1, rollMax = 2; //chanceToGetHit is the number that has to be beaten by the generator in order for the plane to calculate a hit
                                                 //rollMax is the maximum number the generator can roll (the random.range runs on the top number being exclusive so if the numbers are 0,2 it can generate 1 or 0, but not 2)
-    
+
     public int damageChance = 2; //this is the number you add in order to calculate if the plane takes damage during a hit. Currently it adds to the amount of armor the plane has in the random.range function
                                  //making the top number X bigger than the amount of armor. If the number generated is bigger than the amount of armor it takes damage. 
-    public int damageMultiplier = 5;
+    public int damageMultiplier = 10;
+    public float damageForNonCrit = 1;
+    public float damageForCrit = 5;
     public int killNum = 15;
     public bool hasStarted = false;
     public int roundsSurvived = 0;
@@ -28,7 +30,7 @@ public class PlaneHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -47,7 +49,7 @@ public class PlaneHandler : MonoBehaviour
         {
             int average = totalRounds / 100;
             Debug.Log("This configurations average was " + average);
-            
+
         }
     }
 
@@ -64,30 +66,51 @@ public class PlaneHandler : MonoBehaviour
         int randHit = Random.Range(0, rollMax);
         int randHitArea = Random.Range(0, armorArray.Length);
         int randDamage = Random.Range(0, armorArray[randHitArea] + damageChance);
+        //if (randHit >= chanceToGetHit)
+        //{
+        //    if (armorArray[randHitArea] < randDamage)
+        //    {
+        //        if (armorArray[randHitArea] == 0)
+        //        {
+        //            damageArray[randHitArea] += damageForNonCrit;
+        //        }
+        //        else
+        //        {
+        //            damageArray[randHitArea] += (damageForNonCrit / armorArray[randHitArea]);
+        //        }
+        //    }
+        //}
+
         if (randHit >= chanceToGetHit)
         {
-            if (armorArray[randHitArea] < randDamage)
+            if (armorArray[randHitArea] == 0 && randHitArea != 0 && randHitArea != 2/*has no armor && is not critical*/)
             {
-                damageArray[randHitArea] += 0.5f;
+                //Take normal damage
+                damageArray[randHitArea] += damageForNonCrit;
+            }
+            else if (/*has no armor && is critical*/armorArray[randHitArea] == 0 && (randHitArea == 0 || randHitArea == 2))
+            {
+                //Take enhanced damage
+                damageArray[randHitArea] += damageForCrit;
+            }
+            else if (/*is not critical*/randHitArea != 0 && randHitArea != 2)
+            {
+                //Take normal damage divided by amount of armor
+                damageArray[randHitArea] += (damageForNonCrit / armorArray[randHitArea]); 
+            }
+            else if (/*is critical*/randHitArea == 0 || randHitArea == 2)
+            {
+                //Take enhanced damage divided by armor
+                damageArray[randHitArea] += (damageForCrit / armorArray[randHitArea]);
             }
         }
-        
+
         float dmgNum = 0;
-        
+
         for (int i = 0; i < damageArray.Length; i++)
         {
-            float dmgToAdd = 0;
-
-            if (i == 0 || i == 2)
-            {
-                dmgToAdd += damageArray[i] * damageMultiplier;
-            }
-            else
-            {
-                dmgToAdd += damageArray[i];
-            }
-
-            dmgNum += dmgToAdd;
+            Debug.Log("location " + i + " has taken " + damageArray[i] + " damage");
+            dmgNum += damageArray[i];          
         }
 
         if (dmgNum > killNum)
@@ -103,7 +126,40 @@ public class PlaneHandler : MonoBehaviour
             totalRounds += roundsSurvived;
             roundsSurvived = 0;
         }
-        
+
+    }
+
+    public void DamageTester()
+    {
+        if (/*gets hit*/true)
+        {
+            if (/*has no armor && is not critical*/true)
+            {
+                //Take normal damage
+            }
+            else if (/*has no armor && is critical*/true)
+            {
+                //Take enhanced damage
+            }
+            else if (/*is not critical*/true)
+            {
+                //Take normal damage divided by amount of armor
+            }
+            else if (/*is critical*/true)
+            {
+                //Take enhanced damage divided by armor
+            }
+        }
+
+        for (int i = 0; i < /*armor locations aka 10*/10; i++)
+        {
+            //Add amount of damage taken to a total num
+        }
+
+        if (/*amount of damage taken is greated than killNum, plane crashes*/true)
+        {
+
+        }
     }
 
     public void DamageArrayResetter()
